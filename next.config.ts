b9b5ad4c31e16917cf2d835a0a0a01f9e63d8a2f
Ilyakar <ilyakar@gmail.com>
2025-05-17 16:12:01 +0700

    Revert "Tweaks to next.config.ts to try to get Vercel to work"
    This reverts commit 98b137168b7fd2d1651ae9bbb559bd506018e35e.

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
-  webpack: (config: any) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve?.alias,
-        '@': path.resolve(__dirname, 'src'),
-        '@components': path.resolve(__dirname, 'src/app/components'),
-        '@redux': path.resolve(__dirname, 'src/redux'),
-        '@styles': path.resolve(__dirname, 'src/styles')
-      }
-    }
-    return config
-  }
+  /* config options here */
 }
 
 export default nextConfig
